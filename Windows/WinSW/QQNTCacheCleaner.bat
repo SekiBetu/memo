d697f2babdce6b8be618ef3d5922b355e1d39f29
SekiBetu <realsekibetu@gmail.com>
2023-07-28 20:00:04 +0800

    update

---
+++ Windows/WinSW/QQNTCacheCleaner.bat
@@ -0,0 +1,9 @@
+del /f/s/q D:\Cache\QQcache\805023197\nt_qq\nt_data\Pic > nul
+rmdir /s/q D:\Cache\QQcache\805023197\nt_qq\nt_data\Pic
+del /f/s/q D:\Cache\QQcache\805023197\nt_qq\nt_data\Video > nul
+rmdir /s/q D:\Cache\QQcache\805023197\nt_qq\nt_data\Video
+del /f/s/q D:\Cache\QQcache\805023197\nt_qq\nt_data\log > nul
+rmdir /s/q D:\Cache\QQcache\805023197\nt_qq\nt_data\log
+del /f/s/q D:\Cache\QQcache\805023197\nt_qq\nt_data\Emoji\emoji-recv > nul
+rmdir /s/q D:\Cache\QQcache\805023197\nt_qq\nt_data\Emoji\emoji-recv
+exit
